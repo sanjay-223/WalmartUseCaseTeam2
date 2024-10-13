bbafad1f996607b3b1b057ea88125bb959f34968
Barathkumar8610 <146912413+Barathkumar8610@users.noreply.github.com>
2024-10-13 21:20:32 +0530

    Customers with Different Email

---
+++ email.scala
@@ -0,0 +1,11 @@
+val star=spark.read.format("orc").load("/home/labuser/Desktop/star_graph/")
+star.createOrReplaceTempView("star")
+
+val cust=spark.read.format("orc").load("/home/labuser/Desktop/cust_profiles/")
+cust.createOrReplaceTempView("cust")
+
+val partition=spark.sql("select cluster_id,source_system_id,rank() over(partition by cluster_id order by cluster_id) as rank from star")
+partition.createOrReplaceTempView("partition")
+
+val emails=spark.sql("select partition.cluster_id,email_id,count(*),rank() over(partition by cluster_id order by cluster_id) as rank from partition left join cust on partition.source_system_id=cust.source_system_id group by cluster_id,email_id")
+emails.show() 
